127f7bea830861bb540199b3bc44eda385fd52e4
tom_glb <goolMorning_glb@webcode.com>
2019-10-31 16:07:17 +0800

    线程安全的单例 懒汉模式 但开销大，不推荐

---
+++ src/main/java/com/mmall/concurrency/example/singleton/SingletonExample3.java
@@ -0,0 +1,35 @@
+package com.mmall.concurrency.example.singleton;
+
+import com.mmall.concurrency.annoations.NoThreadSafe;
+import com.mmall.concurrency.annoations.ThreadSafe;
+
+/**
+ * @ClassName SingletonExample1
+ * @Description TODO
+ * @Author wushaopei
+ * @Date 2019/10/31 15:54
+ * @Version 1.0
+ */
+/*
+*  懒汉模式
+*  单例实例在第一次使用的时候进行创建
+* */
+@ThreadSafe
+public class SingletonExample3 {
+
+    //私有构造函数
+    private SingletonExample3(){
+
+    }
+
+    //单例对象
+    private static SingletonExample3 instance = null;
+
+    //静态的工厂方法
+    public static synchronized SingletonExample3 getInstance(){
+        if (instance == null){
+            instance = new SingletonExample3();
+        }
+        return instance;
+    }
+}
