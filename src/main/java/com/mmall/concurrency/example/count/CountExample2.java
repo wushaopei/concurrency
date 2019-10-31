5c7e4976b0d1dab02101ab1e800dbf039674fbef
tom_glb <goolMorning_glb@webcode.com>
2019-10-31 10:53:04 +0800

    atomicLong、LongAdder

---
+++ src/main/java/com/mmall/concurrency/example/count/CountExample2.java
@@ -0,0 +1,55 @@
+package com.mmall.concurrency.example.count;
+
+import com.mmall.concurrency.annoations.NoThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @ClassName CountExample1
+ * @Description TODO
+ * @Author wushaopei
+ * @Date 2019/10/30 17:10
+ * @Version 1.0
+ */
+@Slf4j
+@NoThreadSafe
+public class CountExample2 {
+
+    //请求总数
+    public static int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    public static  AtomicInteger count = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0 ; i < clientTotal ; i++){
+            executorService.execute(()->{
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                }catch (Exception e){
+                    log.error("exception",e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}",count.get());
+    }
+
+    private static void  add(){
+        count.incrementAndGet();
+    }
+}
