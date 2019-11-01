90a5377e69b028b44dad5700c436f3dd237d9cda
tom_glb <goolMorning_glb@webcode.com>
2019-11-01 12:42:26 +0800

    请求过滤器

---
+++ src/main/java/com/mmall/concurrency/HttpFilter.java
@@ -0,0 +1,39 @@
+package com.mmall.concurrency;
+
+
+import com.mmall.concurrency.annoations.ThreadSafe;
+import com.mmall.concurrency.example.threadLocal.RequestHolder;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @ClassName HttpFilter
+ * @Description TODO
+ * @Author wushaopei
+ * @Date 2019/11/1 11:18
+ * @Version 1.0
+ */
+@Slf4j
+public class HttpFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        log.info("do filter,{} , {}",Thread.currentThread().getId(),request.getServletPath());
+        RequestHolder.add(Thread.currentThread().getId());
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
